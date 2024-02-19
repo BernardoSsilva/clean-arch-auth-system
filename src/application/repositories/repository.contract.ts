e81e5a3b9c3ab798d224789c88687ff10688a84b
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-19 10:14:53 -0300

    deleted unutilized folders

--- src/application/repositories/repository.contract.ts
+++
@@ -1,9 +0,0 @@
-import { Entity } from 'src/shared/entities/entity';
-
-export interface RepositoryContract<E extends Entity> {
-  findById(id: string): Promise<E>;
-  findAll(): Promise<E[]>;
-  insert(entity: E): Promise<void>;
-  update(id: string, entity: E): Promise<void>;
-  delete(id: string): Promise<void>;
-}
--- src/application/repositories/user.repository.ts
+++
@@ -1,7 +0,0 @@
-import { UserEntity } from '../entities/user.entity';
-import { RepositoryContract } from './repository.contract';
-export interface UserRepositoryInterface
-  extends RepositoryContract<UserEntity> {
-  findByEmail(email: string): Promise<UserEntity>;
-  updatePassword(id: string, password: string): Promise<void>;
-}
