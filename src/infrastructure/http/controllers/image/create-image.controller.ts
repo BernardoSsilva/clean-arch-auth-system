688fb3164ebe7cdd9ee62d019bcfd760d1eecdd0
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-21 16:50:10 -0300

    create image service done

--- src/infrastructure/http/controllers/image/create-image.controller.ts
+++ src/infrastructure/http/controllers/image/create-image.controller.ts
@@ -1,5 +1,4 @@
 import {
-  Body,
   Controller,
   Param,
   Post,
@@ -7,14 +6,28 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { RegisterImageUseCase } from '../../../../application/use-cases/images/register-image.use-case';
 import { ImageEntity } from '../../../../application/entities/image.entity';
+import { RegisterImageUseCase } from '../../../../application/use-cases/images/register-image.use-case';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
+const storage = diskStorage({
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    const randomName = Array(32)
+      .fill(null)
+      .map(() => Math.round(Math.random() * 16).toString(16))
+      .join('');
+    cb(null, `${randomName}`);
+
+    return randomName;
+  },
+});
 @Controller('/image/create')
 export class CreateImageController {
   constructor(private registerImageUseCase: RegisterImageUseCase) {}
   @Post('/:userId')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', { storage }))
   async createImage(
     @UploadedFile() file: Express.Multer.File,
     @Param('userId') userId: string,
@@ -23,7 +36,7 @@ export class CreateImageController {
       imageName: file.originalname,
       imageSize: file.size,
       imageExtension: file.mimetype,
-      imageStoredName: file.originalname,
+      imageStoredName: file.filename,
     });
     const image = await this.registerImageUseCase.execute(createImage, userId);
   }
