9f45e68381ed00083f9065840ba47f68f60e778f
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-21 14:42:26 -0300

    updated database relation type and created image repository

---
+++ src/application/repositories/image.repository.ts
@@ -0,0 +1,14 @@
+import { ImageEntity } from "../entities/image.entity";
+
+
+export abstract class ImageRepository {
+    abstract registerImage(image:ImageEntity):void
+
+    abstract findById(imageId:string):Promise<ImageEntity>
+
+    abstract findAll():Promise<ImageEntity[]>
+
+    abstract findByUserId(userId:string):Promise<ImageEntity>
+
+    abstract deleteImage(imageId:string):Promise<void>
+}
