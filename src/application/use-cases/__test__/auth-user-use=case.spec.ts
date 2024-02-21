5da088d1abfced0aa7f3f8afab88e37c97e2fef9
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-21 11:53:22 -0300

    x

--- src/application/use-cases/__test__/auth-user-use=case.spec.ts
+++ src/application/use-cases/__test__/auth-user-use=case.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryUserRepository } from '../../../../test/repositories/in-memory-user.repository';
 import { AuthUserUseCase } from '../auth-user.use-case';
 import { RegisterUserUseCase } from '../register-user.use-case';
+import * as bcrypt from 'bcrypt';
 
 describe('Authentication use case unit tests', () => {
   it('Should be able to authenticate a user', async () => {
@@ -10,11 +11,11 @@ describe('Authentication use case unit tests', () => {
       userEmail: 'test@email.com',
       userLogin: 'testLogin',
       userName: 'test full name',
-      userPassword: 'testPassword',
+      userPassword: await bcrypt.hash('testPassword', 10),
     });
 
     const loginUseCase = new AuthUserUseCase(userRepository);
-    const loginSuccess = await loginUseCase.execute(createdUser.user.userEmail, createdUser.user.userPassword);
+    const loginSuccess = await loginUseCase.execute(createdUser.user);
     expect(loginSuccess).toBeTruthy();
   });
 });
