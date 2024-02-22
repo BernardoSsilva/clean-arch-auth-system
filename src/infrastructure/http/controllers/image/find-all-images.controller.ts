c942c9b600127f600ec2cd90e4c8dcee123e118d
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-22 11:56:03 -0300

    protected all image routes with jwt token and the auth guard

--- src/infrastructure/http/controllers/image/find-all-images.controller.ts
+++ src/infrastructure/http/controllers/image/find-all-images.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { FindAllImagesUseCase } from '../../../../application/use-cases/images/find-all-images.use-case';
+import { AuthGuard } from 'src/infrastructure/guard/auth.guard';
 
 @Controller('/image')
 export class FindAllImagesController {
   constructor(private findAllImagesUseCase: FindAllImagesUseCase) {}
+  @UseGuards(AuthGuard)
   @Get()
   async findAll() {
-
     const images = await this.findAllImagesUseCase.execute();
     return images;
   }
--- src/infrastructure/http/controllers/image/find-image-by-id.controller.ts
+++ src/infrastructure/http/controllers/image/find-image-by-id.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/infrastructure/guard/auth.guard';
 import { FindImageByIdUseCase } from '../../../../application/use-cases/images/find-image-by-id.use-case';
 
 @Controller('/image')
 export class FindImageByIdController {
   constructor(private findImageByIdUseCase: FindImageByIdUseCase) {}
+  @UseGuards(AuthGuard)
   @Get('/:id')
   async getImageById(@Param('id') id: string) {
     const image = await this.findImageByIdUseCase.execute(id);
-    return image
+    return image;
   }
 }
