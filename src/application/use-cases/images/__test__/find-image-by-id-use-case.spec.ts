bc61f163d9b945b3ef7695f4423f24a854fb61dc
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-22 11:05:20 -0300

    created find image by id use case unit test

---
+++ src/application/use-cases/images/__test__/find-image-by-id-use-case.spec.ts
@@ -0,0 +1,22 @@
+import { ImageEntity } from '../../../../application/entities/image.entity';
+import { ImageInMemoryRepository } from '../../../../../test/repositories/in-memory-image.repository';
+import { FindImageByIdUseCase } from '../find-image-by-id.use-case';
+
+describe('Find image by id use case unit test', () => {
+  const imageRepository = new ImageInMemoryRepository();
+  const findImage = new FindImageByIdUseCase(imageRepository);
+  it('Should be able to find image by id', async () => {
+    const image = new ImageEntity({
+      imageName: 'testName',
+      imageExtension: 'testExtension',
+      imageStoredName: 'testStoredName',
+      imageSize: 500,
+    });
+
+    await imageRepository.registerImage(image, 'testId');
+
+    const result = await findImage.execute('testId');
+
+    expect(result).toEqual(imageRepository.images[0]);
+  });
+});
